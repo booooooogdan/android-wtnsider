0692c3326c2c0bc7fa33e5b9e6c758c1deab2a41
Bogdan <bo7145907@gmail.com>
2020-09-02 23:02:28 +0300

    Добавьте файлы проекта.

---
+++ AndroidWTInsider/MainActivity.cs
@@ -0,0 +1,85 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Com.Syncfusion.Charts;
+using System.Collections.ObjectModel;
+using Android.Graphics;
+
+namespace AndroidWTInsider
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        TextView textMessage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider
+                .RegisterLicense("MzExNTgyQDMxMzgyZTMyMmUzMENBYUR1b2xmRHhjZ2tKblVPWlI5Vm9rYTRSSDlwRDhvalQrbUMzaGhqZXM9");
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+
+            textMessage = FindViewById<TextView>(Resource.Id.message);
+            SfChart chart = FindViewById<SfChart>(Resource.Id.sfChart1);
+
+            chart.Title.Text = "Chart";
+            chart.SetBackgroundColor(Color.White);
+
+            //Initializing primary axis
+            CategoryAxis primaryAxis = new CategoryAxis();
+            primaryAxis.Title.Text = "Name";
+            chart.PrimaryAxis = primaryAxis;
+
+            //Initializing secondary Axis
+            NumericalAxis secondaryAxis = new NumericalAxis();
+            secondaryAxis.Title.Text = "Height (in cm)";
+            chart.SecondaryAxis = secondaryAxis;
+
+            ObservableCollection<ChartData> data = new ObservableCollection<ChartData>()
+            {
+                new ChartData { Name = "David", Height = 180 },
+                new ChartData { Name = "Michael", Height = 170 },
+                new ChartData { Name = "Steve", Height = 160 },
+                new ChartData { Name = "Joel", Height = 182 }
+            };
+
+            //Initializing column series
+            ColumnSeries series = new ColumnSeries();
+            series.ItemsSource = data;
+            series.XBindingPath = "Name";
+            series.YBindingPath = "Height";
+
+            series.DataMarker.ShowLabel = true;
+            series.Label = "Heights";
+            series.TooltipEnabled = true;
+
+            chart.Series.Add(series);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    textMessage.SetText(Resource.String.title_home);
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    textMessage.SetText(Resource.String.title_dashboard);
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    textMessage.SetText(Resource.String.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
